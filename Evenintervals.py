919b44919bdfecea24338f840f88bef17350fdc3
asimeena <42571449+asimeena@users.noreply.github.com>
2018-08-27 20:51:10 +0530

    Update Evenintervals.py

--- Evenintervals.py
+++ Evenintervals.py
@@ -1,9 +1,8 @@
 try:
-	s=int(raw_input())
-	e=int(raw_input())
+	s=int(raw_input());e=int(raw_input())
 	for i in range(s+1,e):
 		if i%2!=0 :
 			continue
 		print(i)
 except:
-	print('invalid')	
+	print('invalid')		
