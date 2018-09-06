9a09831f77a0711783482094e0db0130611d3b70
asimeena <42571449+asimeena@users.noreply.github.com>
2018-09-06 19:23:06 +0530

    Update Min.py

--- Min.py
+++ Min.py
@@ -1,8 +1,2 @@
-N=input()
-list=[]
-for i in range(0,N):
-	int_input=int(raw_input())
-	list.append(int_input)
-for a in list:
-	a=min(list)
-print(a)
+N = raw_input()
+print min(map(int,raw_input().split(" ")))
