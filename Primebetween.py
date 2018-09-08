d04ff7a632716e83bbb7d9bb6b6c6fb0a5ec57ca
asimeena <42571449+asimeena@users.noreply.github.com>
2018-09-08 13:07:08 +0530

    Update Primebetween.py

--- Primebetween.py
+++ Primebetween.py
@@ -1,10 +1,8 @@
-lower = int(raw_input()) 
-upper = int(raw_input())  
- 
-for num in range(lower,upper):  
-   if num > 1:  
-       for i in range(2,num):  
-           if (num % i) == 0:  
-               break  
-       else:  
-           print(num) 
+a,b=map(int,raw_input().split())
+for i in range(a+1,b):
+ if i>1:
+ 	for v in range(2,i):
+ 		if i%v==0:
+ 			break
+ 	else:
+ 		print i,
